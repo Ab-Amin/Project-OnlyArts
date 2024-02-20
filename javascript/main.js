b54d1649039bba759b57ce4c4ff8b4559aa8339e
Ab-Amin <abourrabiaamin@gmail.com>
2024-02-20 13:17:14 +0100

    starting of the project

---
+++ javascript/main.js
@@ -0,0 +1,39 @@
+// key = tiondernse
+
+// processEuropeanaSearch({
+//   "apikey":"tiondernse",
+//   "action":"record.json",
+//   "success":true,
+//   "statsDuration":22,
+//   "requestNumber":8,
+// })
+
+// image --> https://www.artic.edu/iiif/2 + image_id + /full/843,/0/default.jpg
+
+fetch(`https://api.artic.edu/api/v1/artworks?page=1&limit=100`)
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {console.log("Erreur lors de la récup des données :", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
