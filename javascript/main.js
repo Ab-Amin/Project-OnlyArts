270223e04b7fd2cb06ff5e1644db4c2d674e9ad3
Ab-Amin <abourrabiaamin@gmail.com>
2024-02-23 09:56:04 +0100

    création des catégories + buttons favoris&read-more

--- javascript/main.js
+++ javascript/main.js
@@ -19,7 +19,9 @@ let swiper = new Swiper(".mySwiper", {
 });
 
 let swiperWrapper = document.querySelector('.swiper-wrapper')
-let selectCategory = document.querySelector('#select-category')
+let selectCategory = document.querySelector('.select-category')
+let selectCountry = document.querySelector('.select-country')
+let selectDate = document.querySelector('.select-dates')
 let selectBox = document.querySelector('.selections')
 
 
@@ -29,47 +31,106 @@ fetch(`https://api.artic.edu/api/v1/artworks?page=1&limit=100`)
 .then(data => {
   console.log(data);
   // data.data[i].
-  console.log(selectCategory.value);
-  // console.log(data.data.category_titles);
+  // console.log(data.data.department_title);
 
-  // let categoryList = []
-  // const count = [];
-
+  // let categoryListOnebyOne = []
+  // let count = [];
   // for (i = 0; i < data.data.length; i++) {
-  //   if (categoryList.includes(data.data[i].category_titles[i]) == false) {
-  //     categoryList.push(`${data.data[i].category_titles}`)
-  //   }
-    
-  //   if (data.data[i].category_titles == "" || 
-  //       data.data[i].category_titles == null || 
-  //       data.data[i].category_titles == NaN ){
-  //     console.log(`obj ${i} doesn't have a category`);
-  //   }
+  //     categoryListOnebyOne.push(`${data.data[i].department_title}`)
   // }
-  // console.log(categoryList);
-
-  // for (i = 0; i < categoryList.length; i++) {
-  //   const element = categoryList[i];
+  // console.log(categoryListOnebyOne);
+  // for (i = 0; i < categoryListOnebyOne.length; i++) {
+  //   const element = categoryListOnebyOne[i];
   //   count[element] = (count[element] || 0) + 1;
   // }
   // console.log(count);
 
+
+  let categoryList = []
+  function sortByCategory() {
+    for (let i = 0; i < data.data.length; i++) {
+      if (categoryList.includes(data.data[i].department_title) == false) {
+        categoryList.push(`${data.data[i].department_title}`)
+      
+      } else if (data.data[i].department_title == null){
+        console.log(`obj ${i} doesn't have a category`);
+      
+      }
+    }
+    // console.log(categoryList);
+    for (let i = 0; i < categoryList.length; i++) {
+      if (categoryList[i] != "null"){
+        selectCategory.innerHTML += `
+        <option value="${categoryList[i]}">${categoryList[i]}</option>`
+
+      }
+    }
+  }
+  sortByCategory()
+
+  let contryOfOrigin = []
+  function sortByContry() {
+    for (let i = 0; i < data.data.length; i++) {
+      if (contryOfOrigin.includes(data.data[i].place_of_origin) == false) {
+        contryOfOrigin.push(`${data.data[i].place_of_origin}`)
+      
+      } else if (data.data[i].place_of_origin == null){
+        console.log(`country of origin of ${i} is unknown`);
+      
+      }
+    }
+    // console.log(contryOfOrigin);
+    for (let i = 0; i < contryOfOrigin.length; i++) {
+      if (contryOfOrigin[i] != "null"){
+        selectCountry.innerHTML += `
+        <option value="${contryOfOrigin[i]}">${contryOfOrigin[i]}</option>`
+      
+      }
+    }
+  }
+  sortByContry()
+
+  let byDate = []
+  function sortByDate() {
+    for (let i = 0; i < data.data.length; i++) {
+      if (byDate.includes(data.data[i].date_end) == false) {
+        byDate.push(`${data.data[i].date_end}`)
+      
+      } else if (data.data[i].date_end == null){
+        console.log(`Date of end of ${i} is unavailable`);
+      
+      }
+    }
+    // console.log(byDate);
+    for (let i = 0; i < byDate.length; i++) {
+      if (byDate[i] != "null"){
+        selectDate.innerHTML += `
+        <option value="${byDate[i]}">${byDate[i]}</option>`
+      
+      }
+    }
+  }
+  sortByDate()
+
+
   for (let i = 0; i < data.data.length; i++) {
     let artImg = `https://www.artic.edu/iiif/2/${data.data[i].image_id}/full/843,/0/default.jpg`
     
-
-    
-    
     swiperWrapper.innerHTML += `
     <div class="swiper-slide" data-art="${i}" data-category="${data.data[i].category_titles}">
-    <img src="${artImg}" alt="">
-    <button type="button" title="add to favorits" class="add-to-list">
-    <i class="fa-solid fa-plus"></i>
-    </button>
-    <div class="desc">
-    <p>${data.data[i].title}</p>
-    <p>by ${data.data[i].artist_title}</p>
-    </div>
+      <img src="${artImg}" alt="">
+      <div class="buttons">
+        <button type="button" title="add to Favorits" class="add-to-fav">
+          <i class="fa-solid fa-heart"></i>
+        </button>
+        <button type="button" title="Read More..." class="read-more">
+          <i class="fa-solid fa-plus"></i>
+        </button>
+      </div>
+      <div class="desc">
+        <p>${data.data[i].title}</p>
+        <p>by ${data.data[i].artist_title}</p>
+      </div>
     </div>
     `
   }
